6b55c3f675eb9cefb2d33e54d3a02ca86a023e73
yangc <simayifeng@gmail.com>
2023-06-23 23:02:59 +0800

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ available_setting = {
     "single_chat_reply_prefix": "[bot] ",  # 私聊时自动回复的前缀，用于区分真人
     "single_chat_reply_suffix": "",  # 私聊时自动回复的后缀，\n 可以换行    
     "group_chat_prefix": ["@bot"],  # 群聊时包含该前缀则会触发机器人回复 
-    "group_chat_reply_prefix": "",  # 群聊时自动回复的前缀
+    "group_chat_reply_prefix": "[bot]",  # 群聊时自动回复的前缀
     "group_chat_reply_suffix": "",   # 群聊时自动回复的后缀，\n 可以换行
     "group_chat_keyword": [],  # 群聊时包含该关键词则会触发机器人回复
     "group_at_off": False,  # 是否关闭群聊时@bot的触发
